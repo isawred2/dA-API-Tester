cc5472a914ab268415a4dc80ed249ee142ee01a8
Aaron Pearce <me@aaronpearce.com>
2011-06-09 02:31:20 -0700

    Edited run.php via GitHub

--- run.php
+++ run.php
@@ -1,4 +1,4 @@
 <?php
 	require("class.dAAPI.php"); // include the class file
-	$daapi = new dAAPI(##, "##############"); // Construct the dAAPI
+	$daapi = new dAAPI(32, "74681072ca1b0fcf5c8cf4ce1fcad616"); // Construct the dAAPI
 ?>
